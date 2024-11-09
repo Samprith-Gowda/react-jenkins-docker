d9ec4943634f304e4bd3444752b6fc63106fe326
Samprith-Gowda <samprith79@gmail.com>
2024-11-09 15:51:31 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello from Advanced React in Docker!</h1>
+        </div>
+    );
+};
+
+export default App;
