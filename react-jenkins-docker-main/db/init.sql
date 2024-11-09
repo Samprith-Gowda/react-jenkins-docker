d1f287cd8de7bcfbc9112294ed2e29b3477ab277
Samprith-Gowda <samprith79@gmail.com>
2024-11-09 15:52:15 +0530

    Delete react-jenkins-docker-main directory

--- react-jenkins-docker-main/db/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100)
-);
-
-INSERT INTO users (name, email) VALUES ('John Doe', 'johndoe@example.com');
